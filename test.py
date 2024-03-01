110f1dcf343994de66f320e549f8ee1aa1d17ddc
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-03-01 05:28:37 +0000

    Fri, Mar 1, 2024, 5:28 AM +00:00

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import yfinance as yf
 from datetime import datetime, timedelta
+import pandas as pd
 
 # Calculate the start date as 30 days ago from today
 e_d = datetime.today()-timedelta(days=1)
 end_date = e_d.strftime('%Y-%m-%d')
-start_date = (e_d - timedelta(days=7)).strftime('%Y-%m-%d')
+s_d = e_d - timedelta(days=7)
+start_date=s_d.strftime("%Y-%m-%d")
 
 # Fetch historical intraday data for Jio Platforms Limited for the last 30 days
 jiofin_data = yf.download("JIOFIN.NS", start=start_date, end=end_date, interval="1m")
 
-for i in range(0,4):
-    e_d=start_date
-    start_date = (e_d - timedelta(days=7)).strftime('%Y-%m-%d')
+for i in range(0,3):
+    e_d=s_d
+    end_date = e_d.strftime('%Y-%m-%d')
+    s_d = e_d - timedelta(days=7)
+    start_date=s_d.strftime("%Y-%m-%d")
 
     # Fetch historical intraday data for Jio Platforms Limited for the last 30 days
     df_temp = yf.download("JIOFIN.NS", start=start_date, end=end_date, interval="1m")
