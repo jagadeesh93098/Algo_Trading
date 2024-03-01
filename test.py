7f017d13733ddff16f18fdf4754fa821f902fafe
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-03-01 04:44:58 +0000

    Fri, Mar 1, 2024, 4:44 AM +00:00

---
+++ test.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+
+# Calculate the start date as 30 days ago from today
+e_d=datetime.today()-timedelta(days=1)
+end_date = e_d.strftime('%Y-%m-%d')
+start_date = (e_d - timedelta(days=1)).strftime('%Y-%m-%d')
+
+# Fetch historical intraday data for Jio Platforms Limited for the last 30 days
+jiofin_data = yf.download("JIOFIN.NS", start=start_date, end=end_date, interval="1m")
+
+print(jiofin_data.head())
