8459fd07bf1fdd09fc3b47a8f6333608e04abd82
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-03-28 15:34:58 +0000

    Thu, Mar 28, 2024, 9:04 PM +05:30

--- test.py
+++ test.py
@@ -4,36 +4,35 @@ import pandas as pd
 
 # Goal is Calculate Beta of Stocks as I've Entered and Exited my Stake in Them. For Estimating the Expected Returns.
 
-# Calculate the start date as 30 days ago from today
-e_d = datetime.today()-timedelta(days=1)
-end_date = e_d.strftime('%Y-%m-%d')
-s_d = e_d - timedelta(days=7)
-start_date=s_d.strftime("%Y-%m-%d")
-
-# Fetch historical intraday data for Jio Platforms Limited for the last 30 days
-jiofin_data = yf.download("JIOFIN.NS", start=start_date, end=end_date, interval="1m")
-
-for i in range(0,3):
-    e_d=s_d
+def my_data(s):
+    # Calculate the start date as 30 days ago from today
+    e_d = datetime.today()-timedelta(days=1)
     end_date = e_d.strftime('%Y-%m-%d')
     s_d = e_d - timedelta(days=7)
     start_date=s_d.strftime("%Y-%m-%d")
 
     # Fetch historical intraday data for Jio Platforms Limited for the last 30 days
-    df_temp = yf.download("JIOFIN.NS", start=start_date, end=end_date, interval="1m")
-    jiofin_data=pd.concat([df_temp,jiofin_data],axis=0)
+    df = yf.download(s, start=start_date, end=end_date, interval="1m")
 
+    for i in range(0,3):
+        e_d=s_d
+        end_date = e_d.strftime('%Y-%m-%d')
+        s_d = e_d - timedelta(days=7)
+        start_date=s_d.strftime("%Y-%m-%d")
 
-j=yf.Ticker('^NSEI')
-for i in list(j.info.keys()):
-    if "beta"in i:
-        print(i)
+        # Fetch historical intraday data for Jio Platforms Limited for the last 30 days
+        df_temp = yf.download(s, start=start_date, end=end_date, interval="1m")
+        df=pd.concat([df_temp,df],axis=0)
 
+    return df
 
+jiofin_data=my_data("JIOFIN.NS")
+
+nifty_data=my_data("^NSE")
 
 jiofin_data.reset_index(inplace=True,drop=True)
 
 jiofin_data['day_change']=jiofin_data['Close']-jiofin_data['Open']
-jiofin_data['Day_Change']=round(jiofin_data['Day_Change']/jiofin_data['Open'],5)
+jiofin_data['day_change_pu']=round(jiofin_data['day_change']/jiofin_data['Open'],5)
 
 jiofin_data
