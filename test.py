f087e13c24e474283e4544f3b7bcc401c387c3b4
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-03-28 17:20:43 +0000

    Thu, Mar 28, 2024, 10:50 PM +05:30

--- test.py
+++ test.py
@@ -31,13 +31,13 @@ def my_data_day(s):
     # Calculate the start date as 30 days ago from today
     e_d = datetime.today()-timedelta(days=1)
     end_date = e_d.strftime('%Y-%m-%d')
-    s_d = e_d - timedelta(days=30)
+    s_d = e_d - timedelta(days=4*7)
     start_date=s_d.strftime("%Y-%m-%d")
 
     # Fetch historical intraday data for Jio Platforms Limited for the last 30 days
     df = yf.download(s, start=start_date, end=end_date, interval="1d")
 
-    for i in range(0,3):
+    for i in range(0,1):
         e_d=s_d
         end_date = e_d.strftime('%Y-%m-%d')
         s_d = e_d - timedelta(days=7)
