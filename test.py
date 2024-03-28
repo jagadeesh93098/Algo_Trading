4df2a687db0e242130ddbcbbdc7cff7eaf7b2c54
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-03-28 15:39:43 +0000

    Thu, Mar 28, 2024, 9:09 PM +05:30

--- test.py
+++ test.py
@@ -28,11 +28,19 @@ def my_data(s):
 
 jiofin_data=my_data("JIOFIN.NS")
 
-nifty_data=my_data("^NSE")
+nifty_data=my_data("^NSEI")
 
-jiofin_data.reset_index(inplace=True,drop=True)
 
-jiofin_data['day_change']=jiofin_data['Close']-jiofin_data['Open']
-jiofin_data['day_change_pu']=round(jiofin_data['day_change']/jiofin_data['Open'],5)
+def my_day_change(df):
+    df_temp=df.copy()
+    df_temp.reset_index(inplace=True,drop=True)
+    df_temp['day_change']=df_temp['Close']-df_temp['Open']
+    df_temp['day_change_pu']=round(df_temp['day_change']/df_temp['Open'],5)
+    return df_temp
 
-jiofin_data
+
+jiofin_data=my_day_change(jiofin_data)
+
+nifty_data=my_day_change(nifty_data)
+
+
