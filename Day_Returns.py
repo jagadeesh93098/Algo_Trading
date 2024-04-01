aa64b7b03a491506378fe61799153407230f554a
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-04-01 18:47:14 +0000

    Tue, Apr 2, 2024, 12:17 AM +05:30

--- Day_Returns.py
+++ Day_Returns.py
@@ -4,4 +4,24 @@ import pandas as pd
 
 def my_data_day(s):
     e_d=datetime.today()-timedelta(days=1)
-    
+    end_date=e_d.strftime("%Y-%m-%d")
+    s_d=e_d - timedelta(days=30*9)
+    start_date=s_d.strftime("%Y-%m-%d")
+
+    df=yf.download(s,start=start_date,end=end_date,interval='1d')
+
+    return df
+
+def my_profit_prev_close(df):
+    df.reset_index(inplace=True,drop=False)
+    df.loc[:,"change_prev_close"]=0
+    for i in range(1,df.shape[0]):
+        df.loc[i,'change_prev_close']=round((df.loc[i,'Close']-df.loc[i=1,'Close'])/df.loc[i-1,'Close'],5)
+    return df
+
+
+df=my_data_day("JIOFIN.NS")
+
+df=my_profit_prev_close(df)
+
+df
