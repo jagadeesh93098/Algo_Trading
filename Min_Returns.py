af2048076dc8c811f42aee7e8cff01d1e8e79135
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-04-01 19:05:17 +0000

    Tue, Apr 2, 2024, 12:35 AM +05:30

--- Min_Returns.py
+++ Min_Returns.py
@@ -8,7 +8,7 @@ def my_data_min(s):
     s_d=e_d-timedelta(days=7)
     start_date=s_d.strftime("%Y-%m-%d")
 
-    df=yd.download(s,start=start_date,end=end_date,interval='1m')
+    df=yf.download(s,start=start_date,end=end_date,interval='1m')
 
     for i in range(0,3):
         e_d=s_d
@@ -21,14 +21,19 @@ def my_data_min(s):
 
 def my_profit_prev_close(df):
     df.reset_index(inplace=True,drop=False)
-    df.loc[:,"change_prev_close"]=0
-    d
+    df.loc[:,"change_prev_close"]=0.00
     for i in range(1,df.shape[0]):
         df.loc[i,'change_prev_close']=round((df.loc[i,'Close']-df.loc[i-1,'Close'])*100/df.loc[i-1,'Close'],5)
     return df
 
-df=my_data_day('JIOFIN.NS')
+df=my_data_min('JIOFIN.NS')
 
 df=my_profit_prev_close(df)
 
+6*60*df['change_prev_close'].loc[1:].mean()
+6*60*df['change_prev_close'].loc[1:].var()
 
+import matplotlib.pyplot as plt
+
+plt.plot([i for i in range(1,df.shape[0])],df['change_prev_close'].loc[1:])
+plt.show()
