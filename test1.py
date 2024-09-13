c76f52b8765a756623e477e259095ed534d10b81
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-13 15:07:52 +0000

    Fri, Sep 13, 2024, 8:37 PM +05:30

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ import asyncio
 client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI3NTE5ODQzLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.XIKQpgBpDUa6CLjf67FjM-4c6lIfvURzU0Dob6RJIZUv3dyCmsZhXxiKMhSccdvCNdfPctU_vPSa6j8WclAfiA"
 
-instruments = [(1, "1333",15),(0,"13",15)]
+instruments = [(0,"13",15)]
 subscription_code = marketfeed.Ticker
 
 # Usage Example
@@ -19,8 +19,8 @@ async def on_message(instance, message):
 
 print("Subscription code :", subscription_code)
 feed = marketfeed.DhanFeed(client_id, access_token,instruments)
-feed.subscribe_symbols(instruments)
-# feed.run_forever()
 asyncio.create_task(feed.run_forever())
+feed.run_forever()
+feed.subscribe_symbols(instruments)
 feed.disconnect()
 
