ec1c4003e620c14ace5e3ec16cc98e086190fb4e
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-13 14:58:24 +0000

    Fri, Sep 13, 2024, 8:28 PM +05:30

--- test1.py
+++ test1.py
@@ -7,8 +7,10 @@ import asyncio
 client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI3NTE5ODQzLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.XIKQpgBpDUa6CLjf67FjM-4c6lIfvURzU0Dob6RJIZUv3dyCmsZhXxiKMhSccdvCNdfPctU_vPSa6j8WclAfiA"
 
-instruments = [(1, "1333"),(0,"13")]
+instruments = [(1, "1333",15),(0,"13",15)]
 subscription_code = marketfeed.Ticker
+
+
 
 # Usage Example
 async def on_connect(instance):
@@ -18,7 +20,7 @@ async def on_message(instance, message):
     print("Received:", message)
 
 print("Subscription code :", subscription_code)
-feed = marketfeed.DhanFeed(client_id, access_token, instruments, subscription_code,on_connect=on_connect, on_message=on_message)
+feed = marketfeed.DhanFeed(client_id, access_token, instruments)
 
 feed.run_forever()
 
