2203a51448098bb35891bfef5bd5124cfe78ae28
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-16 13:38:24 +0000

    Mon, Sep 16, 2024, 7:08 PM +05:30

--- dhan_market_feed_test.py
+++ dhan_market_feed_test.py
@@ -15,6 +15,8 @@ df['SEM_INSTRUMENT_NAME'].unique()
 
 df['SEM_EXM_EXCH_ID'].unique()
 
+df.loc[(df['SEM_EXM_EXCH_ID'] == 'NSE')]
+
 df.loc[(df['SEM_EXM_EXCH_ID'] == 'MCX') & (df['SEM_TRADING_SYMBOL'].str.startswith('NATURALGAS')) & (df['SEM_STRIKE_PRICE'] == 190),:].head()
 
 instruments = [(5,'430268',15),(5,'436104',15)]
