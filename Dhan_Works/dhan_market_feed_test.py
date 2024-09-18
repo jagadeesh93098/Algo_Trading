bb7330203b6a122ff48616c4b5187f6383404d80
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-18 11:23:46 +0000

    Wed, Sep 18, 2024, 4:53 PM +05:30

--- Dhan_Works/dhan_market_feed_test.py
+++ Dhan_Works/dhan_market_feed_test.py
@@ -1,8 +1,6 @@
 from dhanhq import marketfeed
 import pandas as pd
 
-client_id = "1104088864"
-access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
 
 security_url = "Dhan_Works/security.csv"
 
@@ -45,3 +43,7 @@ start = time.time()
 for i in range(0,10):
     data.get_data()
 print(f"Time Taken = {time.time() - start}")
+
+
+client_id = "1104088864"
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
