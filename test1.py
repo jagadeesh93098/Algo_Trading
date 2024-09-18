236f5da0cedd98695a8078481008400e8e0fb915
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-18 14:49:12 +0000

    Wed, Sep 18, 2024, 8:19 PM +05:30

--- test1.py
+++ test1.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('Dhan_Works/security.csv',low_memory = False)
 
+df.columns
 
+
+df.loc[(df['SEM_INSTRUMENT_NAME'] == 'OPTIDX') & (df['SEM_TRADING_SYMBOL'].str.startswith('NIFTY')) & (df['SEM_CUSTOM_SYMBOL'].str.contains('SEP')),'SEM_EXPIRY_DATE'].unique()
