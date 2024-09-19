dd1972982820c043f59f6113b59ae130e0800090
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-19 13:13:57 +0000

    Thu, Sep 19, 2024, 6:43 PM +05:30

--- test1.py
+++ test1.py
@@ -15,6 +15,8 @@ opt_s_id = df_opt.loc[(df_opt['SEM_STRIKE_PRICE']==190) & (df_opt['SEM_OPTION_TY
 client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
 
+feed = marketfeed.DhanFeed(client_id = client_id, access_token = access_token, instruments = instruments)
+feed.run_forever()
 instruments = [(5,str(opt_s_id),15)]
 
 instruments
@@ -27,27 +29,30 @@ test = pd.DataFrame({'order_id':[],'buyPrice':[],'buyqty':[],'sell_price':[],'lt
 buy_price = ltp
 test.loc[len(test.index)] = ['t1',buy_price,1,None,ltp,None]
 
-feed = marketfeed.DhanFeed(client_id = client_id, access_token = access_token, instruments = instruments)
-feed.run_forever()
 exit = False
 while exit == False:
     response = feed.get_data()
     ltp = eval(response['LTP'])
     test.loc[test['order_id'] == 't1','ltp'] = ltp
-    if ltp-buy_price >= 0.02*buy_price :
+    if ltp-buy_price >= 0.03*buy_price :
         test.loc[test['order_id'] == 't1','sell_price'] = ltp
         print('Sold')
         test.loc[test['order_id'] == 't1','remark'] ='Profit_Booked'
+        print(test)
         exit = True
         break
     if ltp - buy_price <= -1*(0.01)*buy_price:
         test.loc[test['order_id'] == 't1','ltp'] = ltp
         print('Sold')
         test.loc[test['order_id']=='t1','remark'] = 'Stop Loss'
+        print(test)
+        exit = True
         break
     print(test)
     print(f"Present Profit = {(ltp - buy_price)/(buy_price)}")
 
 feed.close_connection()
+
+test
 
 
