f782a267502f20249ea1f573ad07c4bf15f1f051
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-19 12:11:28 +0000

    Thu, Sep 19, 2024, 5:41 PM +05:30

--- test1.py
+++ test1.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from dhanhq import marketfeed
+import requests
 
 df = pd.read_csv('Dhan_Works/security.csv',low_memory = False)
 
+underlying_s_id = df.loc[(df['SEM_EXM_EXCH_ID'] == 'MCX') & (df['SEM_INSTRUMENT_NAME'] == 'FUTCOM') & (df['SEM_TRADING_SYMBOL'].str.startswith('NATURALGAS')) & (df['SEM_EXPIRY_DATE'].str.startswith('2024-09')),'SEM_SMST_SECURITY_ID'].item()
 
+
+
+
+df.loc[(df['SEM_EXM_EXCH_ID'] == 'MCX') & (df['SEM_INSTRUMENT_NAME'] == 'OPTFUT') & (df['SEM_TRADING_SYMBOL'].str.startswith('NATURALGAS')) & (df['SEM_EXPIRY_DATE'].str.startswith('2024-09-23')),:]
