0593e284c6b415bebb02bfdf3356fc09baba5970
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-19 12:04:16 +0000

    Thu, Sep 19, 2024, 5:34 PM +05:30

--- test1.py
+++ test1.py
@@ -1,8 +1,6 @@
 import pandas as pd
+from dhanhq import marketfeed
 
 df = pd.read_csv('Dhan_Works/security.csv',low_memory = False)
 
-df.columns
 
-
-df.loc[(df['SEM_INSTRUMENT_NAME'] == 'FUTIDX') & (df['SEM_TRADING_SYMBOL'].str.startswith('NIFTY')) & (df['SEM_CUSTOM_SYMBOL'].str.contains('SEP')),:]
