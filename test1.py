74cc598d17bac6f3cf047a8a818938455a443990
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-20 14:23:42 +0000

    Fri, Sep 20, 2024, 7:53 PM +05:30

--- test1.py
+++ test1.py
@@ -41,10 +41,10 @@ df_opt
 
 
 
-# my_strike = int(sys.argv[1])
-# my_option_type = str(sys.argv[2])
-my_strike = 5950
-my_option_type = 'CE'
+my_strike = int(sys.argv[1])
+my_option_type = str(sys.argv[2])
+# my_strike = 5950
+# my_option_type = 'CE'
 count = 0
 
 df_opt.loc[(df_opt['SEM_STRIKE_PRICE'] == my_strike) & (df['SEM_OPTION_TYPE']==my_option_type),:]
