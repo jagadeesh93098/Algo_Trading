82b75779d3711eec23bb3fe3c3addc4c8196d4eb
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-20 09:23:13 +0000

    Fri, Sep 20, 2024, 2:53 PM +05:30

--- test1.py
+++ test1.py
@@ -75,7 +75,6 @@ while exit == False:
         present_p = (ltp - buy_price)/buy_price
         if present_p >= p:
             p = present_p + 0.1
-            p_l = present_p - 0.1
         if ltp - buy_price > p*buy_price :
             test.loc[test['order_id'] == 't1','sell_price'] = ltp
             print('Sold')
