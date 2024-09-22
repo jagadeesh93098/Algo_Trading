da7bfab15a3b9da911bebe854b5983ef8921807c
Jagadeesh <jagadeesh93098@gmail.com>
2024-09-23 04:34:49 +0530

    testing_ready

--- Algo_1.py
+++ Algo_1.py
@@ -7,6 +7,13 @@ import time
 import math
 import numpy as np
 
+step = 0.05
+def calculate_qty(fund,ltp):
+    return lot_size * math.floor(fund/(ltp*lot_size))
+
+def calculate_price(price,percent):
+    return step * (math.floor(price*(1+percent)/step)-1)
+
 client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
 
@@ -52,17 +59,9 @@ feed = marketfeed.DhanFeed(client_id = client_id, access_token = access_token, i
 feed.run_forever()
 response = feed.get_data()
 
-response
 ltp = eval(response['LTP'])
-ltp
-
+# ltp = 52.95
 buy_price = ltp
-step = 0.05
-def calculate_qty(fund,ltp):
-    return lot_size * math.floor(fund/(ltp*lot_size))
-
-def calculate_price(price,percent):
-    return step * (math.floor(price*(1+percent)/step)-1)
 
 fund = 10000
 qty = calculate_qty(fund,ltp)
@@ -85,8 +84,6 @@ tpp = (tp_price - buy_price)/buy_price
 trigger = sl_price + step
 pp=0
 
-test = pd.DataFrame({'order_id':[order_id],'BUY_PRICE':[buy_price],'QTY':[qty],'LTP':[ltp],'PP':[pp],'SLP': [slp],"TPP":[tpp],"SL_PRICE":[sl_price],'TRIGGER_PRICE':[trigger],'TP_PRICE':[tp_price],"SELL_PRICE":[np.nan],"P/L":[np.nan]})
-test
 # abs(pt - presentp)/abs(present_p - p)
 
 # Open the Loop
@@ -94,6 +91,8 @@ test
 # Set the Stop Loss based on Present Profit. 
 # If Number of Order Modificate increases 25, Cancel the order any place a new order. 
 
+test = pd.DataFrame({'LTT':[datetime.datetime.now()],'order_id':[order_id],'BUY_PRICE':[buy_price],'QTY':[qty],'LTP':[ltp],'PP':[pp],'SLP': [slp],"TPP":[tpp],"SL_PRICE":[sl_price],'TRIGGER_PRICE':[trigger],'TP_PRICE':[tp_price],"SELL_PRICE":[np.nan],"P/L":[np.nan]})
+test
 
 exit = False
 while exit == False:
@@ -103,7 +102,7 @@ while exit == False:
     if ltp <= trigger:
         sell_price = ltp
         exit = True
-        test.loc[len(test.index),:] = [order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,sl_price,(sl_price - buy_price)*qty]
+        test.loc[len(test.index),:] = [datetime.datetime.now(),order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,sl_price,(sl_price - buy_price)*qty]
         break
     if pp >= tpp:
         tpp = pp + 0.05
@@ -112,6 +111,7 @@ while exit == False:
     trigger = sl_price + step
     slp = (sl_price - buy_price)/buy_price
     tp_price = calculate_price(buy_price,tpp)
-    test.loc[len(test.index),:] = [order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,np.nan,np.nan]
+    test.loc[len(test.index),:] = [datetime.datetime.now(),order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,np.nan,np.nan]
 df.to_csv(file_name,index=False)
+print(df)
 feed.close_connection()
