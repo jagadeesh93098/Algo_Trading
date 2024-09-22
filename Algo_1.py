687f8947473424568a556a3201b19a17c725924b
Jagadeesh <jagadeesh93098@gmail.com>
2024-09-23 04:29:57 +0530

    algo1_testing_ready

--- Algo_1.py
+++ Algo_1.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from dhanhq import marketfeed
-import requests
+# import requests
 import datetime
 import sys
 import time
+import math
+import numpy as np
 
 client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
 
 """ IF the file is not present use the Below Link """
-url = "https://images.dhan.co/api-data/api-scrip-master.csv"
+url = "GIT/cs50dev/security.csv"
 
 df = pd.read_csv(url,low_memory = False)
 
@@ -19,9 +21,9 @@ today_date = datetime.datetime.strftime(today,'%Y-%m-%d')
 
 today_date
 
-underlying = 'CRUDE'
-exch = 'MCX'
-instrument = 'OPTFUT'
+# underlying = 'CRUDE'
+# exch = 'MCX'
+# instrument = 'OPTFUT'
 # instrument = 'FUTCOM'
 
 # df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith('2024-09')) ,:]
@@ -32,7 +34,7 @@ underlying = 'MIDCPNIFTY'
 exch = 'NSE'
 instrument = 'OPTIDX'
 
-df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:]
+# df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:]
 
 df_opt = df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:].copy()
 
@@ -40,10 +42,11 @@ df_opt
 
 # my_strike = int(sys.argv[1])
 # my_option_type = str(sys.argv[2])
-my_strike = 13126
-my_option_type = 'CE'
-df_opt.loc[(df_opt['SEM_STRIKE_PRICE'] == my_strike) & (df['SEM_OPTION_TYPE']==my_option_type),:]
-opt_sid = df_opt.loc[(df_opt['SEM_STRIKE_PRICE'] == my_strike) & (df['SEM_OPTION_TYPE'] == my_option_type) & (df['SM_SYMBOL_NAME'] == 'CRUDEOIL'),'SEM_SMST_SECURITY_ID'].item()
+my_strike = 13100
+my_option_type = 'PE'
+my_option = df_opt.loc[(df_opt['SEM_STRIKE_PRICE'] == my_strike) & (df['SEM_OPTION_TYPE']==my_option_type),:]
+opt_sid = my_option.loc[:,'SEM_SMST_SECURITY_ID'].item()
+lot_size = my_option.loc[:,'SEM_LOT_UNITS'].item()
 instruments = [(5,str(opt_sid),21)]
 feed = marketfeed.DhanFeed(client_id = client_id, access_token = access_token, instruments = instruments)
 feed.run_forever()
@@ -54,15 +57,37 @@ ltp = eval(response['LTP'])
 ltp
 
 buy_price = ltp
-test = pd.DataFrame({'order_id':[],'buyPrice':[],'buyqty':[],'sell_price':[],'ltp':[],'remark' : []})
-test.loc[len(test.index)] = ['t1',buy_price,1,None,ltp,None]
+step = 0.05
+def calculate_qty(fund,ltp):
+    return lot_size * math.floor(fund/(ltp*lot_size))
+
+def calculate_price(price,percent):
+    return step * (math.floor(price*(1+percent)/step)-1)
+
+fund = 10000
+qty = calculate_qty(fund,ltp)
+
+file_name = f'test_order_{datetime.datetime.strftime(today,'%Y_%m_%d')}.csv'
+try :
+    order_list = pd.read_csv(file_name)
+    o=order_list.loc[len(order_list.index),'order_id'].item()
+    order_id = f"O_{int(o.split('_')[1])+1}"
+except FileNotFoundError:
+    order_id = 'O_1'
+
+i_tpp = 0.10
+i_slp = -0.05
+
+sl_price = calculate_price(buy_price,i_slp)
+tp_price = calculate_price(buy_price,i_tpp)
+slp = (sl_price - buy_price)/buy_price
+tpp = (tp_price - buy_price)/buy_price
+trigger = sl_price + step
+pp=0
+
+test = pd.DataFrame({'order_id':[order_id],'BUY_PRICE':[buy_price],'QTY':[qty],'LTP':[ltp],'PP':[pp],'SLP': [slp],"TPP":[tpp],"SL_PRICE":[sl_price],'TRIGGER_PRICE':[trigger],'TP_PRICE':[tp_price],"SELL_PRICE":[np.nan],"P/L":[np.nan]})
 test
 # abs(pt - presentp)/abs(present_p - p)
-
-p_t = 0.10
-p_l = -0.05
-
-d_test = pd.DataFrame({'TNumber':[0],'SL' : [-0.05],'TP':[0.10],'PP':[0.00]})
 
 # Open the Loop
 # Keep Tracking LTP
@@ -70,34 +95,23 @@ d_test = pd.DataFrame({'TNumber':[0],'SL' : [-0.05],'TP':[0.10],'PP':[0.00]})
 # If Number of Order Modificate increases 25, Cancel the order any place a new order. 
 
 
-for i in range(0,25):
-    exit = False
-    while exit == False:
-        response = feed.get_data()
-        ltp = eval(response['LTP'])
-        test.loc[test['order_id'] == 't1','ltp'] = ltp
-        present_p = (ltp - buy_price)/buy_price
-        if present_p >= p_t:
-            p_t = present_p + 0.001
-        p_l = max((3*present_p - p_t)/2, p_l)
-        if ltp - buy_price > p_t*buy_price :
-            test.loc[test['order_id'] == 't1','sell_price'] = ltp
-            print('Sold')
-            test.loc[test['order_id'] == 't1','remark'] =f'Profit_Booked at {p_t}'
-            print(test)
-            exit = True
-            break
-        if ltp - buy_price < p_l*buy_price:
-            test.loc[test['order_id'] == 't1','ltp'] = ltp
-            print('Sold based on Stop Loss')
-            print(f"Profit/Loss Booked = {p_l}")
-            test.loc[test['order_id']=='t1','remark'] = 'Stop Loss'
-            print(test)
-            exit = True
-            break
-        print(test)
-        print(f"Present Profit = {(ltp - buy_price)/(buy_price)}")
-        print(f"Present Stop Loss = {p_l}")
-        print(f"Present Target Profit = {p_t}")
-
+exit = False
+while exit == False:
+    response = feed.get_data()
+    ltp = eval(response['LTP'])
+    pp = (ltp - buy_price)/buy_price
+    if ltp <= trigger:
+        sell_price = ltp
+        exit = True
+        test.loc[len(test.index),:] = [order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,sl_price,(sl_price - buy_price)*qty]
+        break
+    if pp >= tpp:
+        tpp = pp + 0.05
+    slp_temp = max(slp,(3*pp - tpp)/2)
+    sl_price = calculate_price(buy_price,slp)
+    trigger = sl_price + step
+    slp = (sl_price - buy_price)/buy_price
+    tp_price = calculate_price(buy_price,tpp)
+    test.loc[len(test.index),:] = [order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,np.nan,np.nan]
+df.to_csv(file_name,index=False)
 feed.close_connection()
