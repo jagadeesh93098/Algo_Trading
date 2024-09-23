8014b77c30c47d37a854b0c6ce4b579f909760ef
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2024-09-23 10:58:20 +0000

    updated Algo_1

--- Algo_1.py
+++ Algo_1.py
@@ -18,7 +18,7 @@ client_id = "1104088864"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI5MTExMjIxLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNDA4ODg2NCJ9.COQOjTvQ0Cmmjs660wwgYd1jnmi34_wla-keJue08L0-Gv4kGarBedXHOJ9i06kRprRqZOM4u1NtLleZcbKSRQ"
 
 """ IF the file is not present use the Below Link """
-url = "GIT/cs50dev/security.csv"
+url = "security.csv"
 
 df = pd.read_csv(url,low_memory = False)
 
@@ -37,24 +37,31 @@ today_date
 # underlying_sid = df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith('2024-09')) ,'SEM_SMST_SECURITY_ID']
 # underlying_sid
 
-underlying = 'MIDCPNIFTY'
-exch = 'NSE'
+underlying = sys.argv[1]
+# underlying = 'BANKEX'
 instrument = 'OPTIDX'
 
 # df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:]
 
-df_opt = df.loc[(df['SEM_EXM_EXCH_ID'] == exch) & (df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:].copy()
+df_opt = df.loc[(df['SEM_INSTRUMENT_NAME'] == instrument) & (df['SEM_TRADING_SYMBOL'].str.startswith(underlying)) & (df['SEM_EXPIRY_DATE'].str.startswith(today_date)),:].copy()
 
 df_opt
 
-# my_strike = int(sys.argv[1])
-# my_option_type = str(sys.argv[2])
-my_strike = 13100
-my_option_type = 'PE'
+my_strike = int(sys.argv[2])
+my_option_type = str(sys.argv[3])
+
+# my_strike = 13150
+# my_option_type = 'PE'
+
+
+
 my_option = df_opt.loc[(df_opt['SEM_STRIKE_PRICE'] == my_strike) & (df['SEM_OPTION_TYPE']==my_option_type),:]
 opt_sid = my_option.loc[:,'SEM_SMST_SECURITY_ID'].item()
 lot_size = my_option.loc[:,'SEM_LOT_UNITS'].item()
-instruments = [(5,str(opt_sid),21)]
+if underlying == 'MIDCPNIFTY':
+    instruments = [(2,str(opt_sid),15)]
+else:
+    instruments = [(8,str(opt_sid),15)]
 feed = marketfeed.DhanFeed(client_id = client_id, access_token = access_token, instruments = instruments)
 feed.run_forever()
 response = feed.get_data()
@@ -69,13 +76,13 @@ qty = calculate_qty(fund,ltp)
 file_name = f'test_order_{datetime.datetime.strftime(today,'%Y_%m_%d')}.csv'
 try :
     order_list = pd.read_csv(file_name)
-    o=order_list.loc[len(order_list.index),'order_id'].item()
+    o=order_list.loc[len(order_list.index)-1,'order_id']
     order_id = f"O_{int(o.split('_')[1])+1}"
 except FileNotFoundError:
     order_id = 'O_1'
 
-i_tpp = 0.10
-i_slp = -0.05
+i_tpp = float(sys.argv[4])
+i_slp = float(sys.argv[5])
 
 sl_price = calculate_price(buy_price,i_slp)
 tp_price = calculate_price(buy_price,i_tpp)
@@ -88,12 +95,12 @@ pp=0
 
 # Open the Loop
 # Keep Tracking LTP
-# Set the Stop Loss based on Present Profit. 
-# If Number of Order Modificate increases 25, Cancel the order any place a new order. 
+# Set the Stop Loss based on Present Profit.
+# If Number of Order Modificate increases 25, Cancel the order any place a new order.
 
 test = pd.DataFrame({'LTT':[datetime.datetime.now()],'order_id':[order_id],'BUY_PRICE':[buy_price],'QTY':[qty],'LTP':[ltp],'PP':[pp],'SLP': [slp],"TPP":[tpp],"SL_PRICE":[sl_price],'TRIGGER_PRICE':[trigger],'TP_PRICE':[tp_price],"SELL_PRICE":[np.nan],"P/L":[np.nan]})
 test
-
+print(test.loc[[len(test.index)-1],:])
 exit = False
 while exit == False:
     response = feed.get_data()
@@ -102,16 +109,28 @@ while exit == False:
     if ltp <= trigger:
         sell_price = ltp
         exit = True
-        test.loc[len(test.index),:] = [datetime.datetime.now(),order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,sl_price,(sl_price - buy_price)*qty]
+        test.loc[len(test.index),:] = [datetime.datetime.now(),order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,sl_price,(sl_price - buy_price)*qty - 40]
+        print("\n Stop Loss HIT \n")
+        print(test.loc[[len(test.index)-1],:])
         break
     if pp >= tpp:
         tpp = pp + 0.05
     slp_temp = max(slp,(3*pp - tpp)/2)
-    sl_price = calculate_price(buy_price,slp)
+    # print(slp_temp)
+    sl_price = max(sl_price,calculate_price(buy_price,slp_temp))
     trigger = sl_price + step
     slp = (sl_price - buy_price)/buy_price
     tp_price = calculate_price(buy_price,tpp)
     test.loc[len(test.index),:] = [datetime.datetime.now(),order_id,buy_price,qty,ltp,pp,slp,tpp,sl_price,trigger,tp_price,np.nan,np.nan]
-df.to_csv(file_name,index=False)
-print(df)
+    print(test.loc[[len(test.index)-1],:])
+result = test.loc[[len(test.index)-1],:].copy()
+result.reset_index(inplace=True,drop=True)
+list(result.loc[0,:])
+try :
+    order_list = pd.read_csv(file_name)
+    order_list.loc[len(order_list.index),:]=list(result.loc[0,:])
+    print(order_list)
+    order_list.to_csv(file_name,index=False)
+except FileNotFoundError:
+    result.to_csv(file_name,index=False)
 feed.close_connection()
